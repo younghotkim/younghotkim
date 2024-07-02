d338f5db338991734210e677e261b57eec414c30
younghotkim <younghotkim@gmail.com>
2024-07-02 16:32:16 +0900

    day4

---
+++ day4/dom.ts
@@ -0,0 +1,9 @@
+let pPhysicalDOM = document.createElement("p");
+pPhysicalDOM.innerText = "Hello PhysicalDOM world!";
+
+document.body.appendChild(pPhysicalDOM);
+
+let spanPhysicalDOM = document.createElement("span");
+spanPhysicalDOM.innerText = "Hello span world!";
+
+document.body.appendChild(spanPhysicalDOM);
