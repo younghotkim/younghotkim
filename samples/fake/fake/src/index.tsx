6850b92f9e2c7fe5622a700507d08ebf2cbd88fc
younghotkim <younghotkim@gmail.com>
2024-07-02 16:56:33 +0900

    react sample

--- samples/fake/fake/src/index.tsx
+++ samples/fake/fake/src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import * as D from "./data";
+import App from "./App";
+
+// const children = [0, 1, 2].map((n: number) => <h3>Hello world! {n}</h3>);
+
+const children = D.makeArray(10).map((notUsed, index) => (
+  <div key={index}>
+    <p>{D.randomId()}</p>
+    <p>{D.randomName()}</p>
+    <p>{D.randomJobTitle()}</p>
+    <p>{D.randomSentence()}</p>
+    <img src={D.randomAvatar()} width={100} height={100} />
+  </div>
+));
+const rootVirtualDOM = <div>{children}</div>;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(rootVirtualDOM);
