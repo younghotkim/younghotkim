f7d0171717542bfa500505f5f361a38025ec0894
younghotkim <younghotkim@gmail.com>
2024-07-03 12:45:39 +0900

    day5 / class component

---
+++ day5/class-components/src/App.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+import Greeting from "./GreetingFunction";
+
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      enteredName: "",
+      message: "",
+    };
+
+    this.onChangeName = this.onChangeName.bind(this);
+  }
+
+  state: { enteredName: string; message: string };
+
+  onChangeName(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      enteredName: e.target.value,
+      message: `Hello from, ${e.target.value}`,
+    });
+  }
+
+  render() {
+    console.log("rendering App");
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <input value={this.state.enteredName} onChange={this.onChangeName} />
+          <Greeting message={this.state.enteredName} />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ day5/class-components/src/Greeting.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+interface GreetingProps {
+  name?: string;
+}
+
+interface GreetingState {
+  message: string;
+}
+
+export default class Greeting extends React.Component<
+  GreetingProps,
+  GreetingState
+> {
+  constructor(props: GreetingProps) {
+    super(props);
+    this.state = {
+      message: `Hello from, ${props.name}`,
+    };
+  }
+
+  static getDerivedFromProps(props: GreetingProps, state: GreetingState) {
+    console.log(props, state);
+    if (props.name && props.name !== state.message) {
+      const newState = { ...state };
+      newState.message = Greeting.getNewMessage(props.name);
+      return newState;
+    }
+    return state;
+  }
+
+  static getNewMessage(name: string = "") {
+    return `Hello from, ${name}`;
+  }
+
+  render() {
+    console.log("rendering Greeting");
+    if (!this.props.name) {
+      return <div>no name given</div>;
+    }
+
+    return <div>{this.state.message}</div>;
+  }
+}
+function getNewMessage(name: string) {
+  throw new Error("Function not implemented.");
+}
