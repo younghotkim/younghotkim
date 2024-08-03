4fed801c59e3d24fbfcae3105debbeb198741c2d
younghotkim <younghotkim@gmail.com>
2024-08-03 16:46:05 +0900

    0803

--- day22/nodebird-api/controllers/v1.js
+++ day22/nodebird-api/controllers/v1.js
@@ -1,5 +1,5 @@
-const jwt = require('jsonwebtoken');
-const { Domain, User } = require('../models');
+const jwt = require("jsonwebtoken");
+const { Domain, User } = require("../models");
 
 exports.createToken = async (req, res) => {
   const { clientSecret } = req.body;
@@ -8,32 +8,36 @@ exports.createToken = async (req, res) => {
       where: { clientSecret },
       include: {
         model: User,
-        attribute: ['nick', 'id'],
+        attribute: ["nick", "id"],
       },
     });
     if (!domain) {
       return res.status(401).json({
         code: 401,
-        message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
+        message: "등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요",
       });
     }
-    const token = jwt.sign({
-      id: domain.User.id,
-      nick: domain.User.nick,
-    }, process.env.JWT_SECRET, {
-      expiresIn: '1m', // 1분
-      issuer: 'nodebird',
-    });
+    const token = jwt.sign(
+      {
+        id: domain.User.id,
+        nick: domain.User.nick,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "2m", // 1분
+        issuer: "nodebird",
+      }
+    );
     return res.json({
       code: 200,
-      message: '토큰이 발급되었습니다',
+      message: "토큰이 발급되었습니다",
       token,
     });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
       code: 500,
-      message: '서버 에러',
+      message: "서버 에러",
     });
   }
 };
