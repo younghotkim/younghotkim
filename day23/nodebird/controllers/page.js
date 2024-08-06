748e33f51408345a0b36132efebbfc065ed8673f
younghotkim <younghotkim@gmail.com>
2024-08-06 14:45:17 +0900

    0806/pagination

--- day23/nodebird/controllers/page.js
+++ day23/nodebird/controllers/page.js
@@ -1,36 +1,73 @@
-const { User, Post, Hashtag } = require('../models');
+const { User, Post, Hashtag } = require("../models");
 
 exports.renderProfile = (req, res) => {
-  res.render('profile', { title: '내 정보 - NodeBird' });
+  res.render("profile", { title: "내 정보 - NodeBird" });
 };
 
 exports.renderJoin = (req, res) => {
-  res.render('join', { title: '회원가입 - NodeBird' });
+  res.render("join", { title: "회원가입 - NodeBird" });
 };
 
 exports.renderMain = async (req, res, next) => {
+  //paging 구현 할 예정
+  let pageSize = 10;
+  let pagePaddingSize = 2;
+
+  let currentPage = 0;
+
+  if (req.query.currentPage) currentPage = Number(req.query.currentPage);
+
+  // 데이터베이스 쿼리해서 계산
+  let totalSize = 0;
+
   try {
     const posts = await Post.findAll({
       include: {
         model: User,
-        attributes: ['id', 'nick'],
+        attributes: ["id", "nick"],
       },
-      order: [['createdAt', 'DESC']],
     });
-    res.render('main', {
-      title: 'NodeBird',
-      twits: posts,
-    });
+
+    totalSize = posts.length;
   } catch (err) {
     console.error(err);
     next(err);
   }
-}
+
+  let offset = (currentPage - 1) * pageSize;
+  if (offset < 0) {
+    offset = 0;
+  }
+
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ["id", "nick"],
+      },
+      offset: Number(offset) || 0,
+      limit: pageSize,
+      order: [["createdAt", "DESC"]],
+    });
+    res.render("main", {
+      title: "NodeBird",
+      twits: posts,
+      totalSize: totalSize,
+      currentPage: currentPage,
+      pageSize: pageSize,
+      pagePaddingSize: pagePaddingSize,
+    });
+    totalSize = posts.length;
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
 
 exports.renderHashtag = async (req, res, next) => {
   const query = req.query.hashtag;
   if (!query) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   try {
     const hashtag = await Hashtag.findOne({ where: { title: query } });
@@ -39,7 +76,7 @@ exports.renderHashtag = async (req, res, next) => {
       posts = await hashtag.getPosts({ include: [{ model: User }] });
     }
 
-    return res.render('main', {
+    return res.render("main", {
       title: `${query} | NodeBird`,
       twits: posts,
     });
